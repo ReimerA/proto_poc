747e734fbbd0d42ca62779eba99a9a717a9651d3
Anders Reimer <anrem@vestas.com>
2023-05-01 13:32:17 +0200

    added reverse serialization

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿
 
 using ProtoBuf;
+using Google.Protobuf;
 
 //Generate .proto file from type
 //  string proto = Serializer.GetProto<DataChangedPubMsg>();
@@ -8,7 +9,8 @@ using ProtoBuf;
 
 
 	var msgObj = new DataChangedPubMsg() { DateTime=DateTime.Now, Amount=123.456M};
-	Console.WriteLine($"object for protobuf-net serialization: {msgObj.DateTime.Ticks}");
+	Console.WriteLine($"object DateTime ticks for protobuf-net serialization: {msgObj.DateTime.Ticks}");
+	Console.WriteLine($"object Decimal for protobuf-net serialization: {msgObj.DateTime.Ticks}");
 
 	byte[] msgbytes;
 	using (var ms = new MemoryStream())
@@ -18,9 +20,21 @@ using ProtoBuf;
 	}
 	
 	var outMsg = Google.Protobuf.TestProto.DataChangedPubMsg.Parser.ParseFrom(msgbytes);
+
     Console.WriteLine($"Google Deserialized ticks value: {outMsg.DateTime.Value}");
     Console.WriteLine($"Google Deserialized Decimal Hi value: {outMsg.Amount.Hi}");
     Console.WriteLine($"Google Deserialized Decimal Lo value: {outMsg.Amount.Lo}");
 
+	byte[] msgbytesGoogleSource;
+	using (var ms = new MemoryStream())
+	{
+		outMsg.WriteTo(ms);
+		msgbytesGoogleSource = ms.ToArray();
+	}
+
+	var outMsg2 = Serializer.Deserialize<DataChangedPubMsg>(msgbytesGoogleSource.AsSpan());
+
 	
+    Console.WriteLine($"Protobuf-net Deserialized back into DateTime ticks value: {outMsg2.DateTime.Ticks}");
+    Console.WriteLine($"Protobuf-net Deserialized back into Decimal value: {outMsg2.Amount}");
 	//outMsg.Dump();
